0cd668b89e223ada52f255595d257c3507951aa4
Eliza Meeks <theartstudio@Elizas-MacBook-Pro.local>
2017-05-24 13:02:56 -0500

    Handlebars stuff

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,40 @@
 "use strict";
 
+let Handlebars = require('hbsfy/runtime'),
+	mdb = require('./mdb-loader.js'),
+	moviesTemplate = require('../templates/populatemovies.hbs');
 
+let comboObj = [];
+
+let loadMoviesToDom = () => {
+	mdb.getPopular().
+	then(function(songData){
+		console.log("popular", songData);
+		songData.forEach(function(element){
+			var newObj = {};
+			newObj.movie = element.title;
+			newObj.year = element.year;
+			mdb.getCredits(element.id)
+			.then(function(actors){
+				newObj.cast = actors;
+				comboObj.push(newObj);
+			});
+		});
+		// console.log("comboObj", comboObj);
+	}).
+	then(function(){
+		console.log("comboObj", comboObj);
+		console.log("movie", comboObj[0]);
+		$("#outputArea").html(moviesTemplate(comboObj));
+	});
+};
+let fivefifty = "550";
+let getCreditData = (movieId) => {
+	mdb.getCredits(movieId)
+	.then(function(creditData){
+		console.log("creditData", creditData);
+	});
+};
+loadMoviesToDom();
+// getCreditData(fivefifty);
+// mdb.creditsURL(fivefifty);
--- javascripts/mdb-loader.js
+++ javascripts/mdb-loader.js
@@ -1,13 +1,22 @@
 "use strict";
 
-let movieDB= require("./mdb-config");
+let movieDB= require("./mdb-config"),
+    moviesArray = [];
 
-function getPopular(data){
+function getPopular(){
     return new Promise(function(resolve,reject){
         $.ajax({
             url: `${movieDB.getMDBsettings().popularURL}`
         }).done(function(movieData){
-            resolve(movieData);
+            movieData.results.forEach(function(element){
+                let movieObj = {
+                    title: `${element.title}`,
+                    year: `${element.release_date}`,
+                    id: `${element.id}`
+                };
+                moviesArray.push(movieObj);
+            });
+            resolve(moviesArray);
         });
     });
 }
@@ -22,8 +31,20 @@ function searchMDB(movieName){
     });
 }
 
+function getCredits (movieId){
+    return new Promise(function(resolve, reject){
+        $.ajax({
+            url: `${movieDB.getMDBsettings().creditURL}${movieId}${movieDB.getMDBsettings().endCreditURL}`
+        }).done(function(credits){
+            let actorsArray = [];
+            for (var i = 0; i < 4; i++){
+                actorsArray.push(credits.cast[i].name);
+            }
+            resolve(actorsArray);
+        });
+    });
+}
 
-
-module.exports= {getPopular, searchMDB};
+module.exports= {getPopular, searchMDB, getCredits};
 
 

8930294b256f91f1293956fd2834d66d2cbf7abd
cleverbek1991 <dilshod@Dilshods-MacBook-Pro.local>
2017-05-24 11:16:18 -0500

    search query

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,13 @@
 "use strict";
 
+let mdb = require('./mdb-loader');
 
+// Get input value and pass it to .. searchMBD
+$(document).on('click', '.find-new-movie', () => {
+	let inputValue = $('.form-control').val();
+	let movieName = inputValue.replace(/ /gi, '+');
+	mdb.searchMDB(movieName)
+	.then((value) => {
+    	console.log('Input value is', movieName);
+	});
+});

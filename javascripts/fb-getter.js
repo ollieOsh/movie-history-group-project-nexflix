a574fc40e09b4c4ac8a231ac2b87b16537d6c2c5
Eliza Meeks <theartstudio@Elizas-MacBook-Pro.local>
2017-05-24 09:16:41 -0500

    firebase getter settings

--- javascripts/fb-getter.js
+++ javascripts/fb-getter.js
@@ -1,2 +1,11 @@
 "use strict";
 
+let getFbKey = () => {
+	return {
+    apiKey: "AIzaSyAaE77JuLDwM7F3OqGFt8kNqiStqiCHkZY",
+    authDomain: "netflix-app-a21ac.firebaseapp.com",
+    databaseURL: "https://netflix-app-a21ac.firebaseio.com",
+  };
+};
+
+module.exports = getFbKey;

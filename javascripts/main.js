8879084a140317c547427f36b5967e5fcd628b5b
Eliza Meeks <theartstudio@Elizas-MacBook-Pro.local>
2017-05-24 14:58:31 -0500

    handlebars styled ok

--- javascripts/main.js
+++ javascripts/main.js
@@ -9,6 +9,21 @@ let comboObj = {
 	movies: []
 };
 
+Handlebars.registerHelper('grouped_each', function(every, context, options) {
+    var out = "", subcontext = [], i;
+    if (context && context.length > 0) {
+        for (i = 0; i < context.length; i++) {
+            if (i > 0 && i % every === 0) {
+                out += options.fn(subcontext);
+                subcontext = [];
+            }
+            subcontext.push(context[i]);
+        }
+        out += options.fn(subcontext);
+    }
+    return out;
+});
+
 let loadMoviesToDom = () => {
 	mdb.getPopular().
 	then(function(songData){

23027539a3c36a140cba49baac8607ccb6c17e9b
Ryan McCarty <Ryan McCarty>
2017-05-24 09:41:32 -0500

    added mdb getter, config, and loader

--- javascripts/mdb-config.js
+++ javascripts/mdb-config.js
@@ -1,2 +1,19 @@
 "use strict";
 
+let mdb= require("./mdb-getter");
+let mdbData= mdb();
+
+var mdbConfig={
+    mdbKey: mdbData.apiKey,
+    searchURL: mdbData.searchURL,
+    popularURL: mdbData.popularURL,
+    creditURL: mdbData.creditURL,
+    endcreditURL: mdbData.endcreditURL,
+    posterURL: mdbData.posterURL
+};
+
+let getMDBsettings= ()=>{
+    return mdbConfig;
+};
+
+module.exports= {getMDBsettings};
--- javascripts/mdb-getter.js
+++ javascripts/mdb-getter.js
@@ -1,2 +1,14 @@
 "use strict";
 
+function getMDBKey(){
+    return {
+        apiKey: "c5c6876138711da8b6618fa62fee6117",
+        searchURL: "https://api.themoviedb.org/3/search/movie?api_key=c5c6876138711da8b6618fa62fee6117&query=",
+        popularURL: "https://api.themoviedb.org/3/movie/popular?api_key=c5c6876138711da8b6618fa62fee6117&language=en-US&page=1",
+        creditURL: "https://api.themoviedb.org/3/movie/",
+        endCreditURL: "/credits?api_key=c5c6876138711da8b6618fa62fee6117",
+        posterURL: "https://image.tmdb.org/t/p/w500/$%7Bposter_path%7D"
+    };
+}
+
+module.exports= getMDBKey;

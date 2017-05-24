be2d640cb890987d39c6598f03969e72e51801d1
Adam Reidelbach <adamreidelbach@gmail.com>
2017-05-24 13:35:04 -0500

    updated wire frame complete

--- javascripts/main.js
+++ javascripts/main.js
@@ -26,3 +26,28 @@ $("#logout").click(function() {
     $("#watchedButtons").addClass("hide");
 });
 
+//user clicks unwatched search filter and breadcrumbs appear
+$("#untracked").click(function() {
+    $("#breadcrumb").html(`<li class="search-results">Search Results</li>`);
+});
+
+//user clicks unwatched search filter and breadcrumbs appear
+$("#unwatched").click(function() {
+    $("#breadcrumb").html(`<li class="search-results">Search Results</li>`);
+    $("#breadcrumb").append(`<li class="search-results">Unwatched</li>`);
+});
+
+//user clicks watched search filter and breadcrumbs appear
+$("#watched").click(function() {
+    $("#breadcrumb").html(`<li class="search-results">Search Results</li>`);
+    $("#breadcrumb").append(`<li class="search-results">Unwatched</li>`);
+    $("#breadcrumb").append(`<li class="search-results">Watched</li>`);
+});
+
+//user clicks favorites search filter and breadcrumbs appear
+$("#favorites").click(function() {
+    $("#breadcrumb").html(`<li class="search-results">Search Results</li>`);
+    $("#breadcrumb").append(`<li class="search-results">Unwatched</li>`);
+    $("#breadcrumb").append(`<li class="search-results">Watched</li>`);
+    $("#breadcrumb").append(`<li class="search-results">Favorites</li>`);
+});

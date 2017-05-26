9a0c2e0566c1520e016553629684d365857f4950
ollieOsh <sosinusi@gmail.com>
2017-05-26 10:57:24 -0500

    buildNewObj gives movie with no poster a default image

--- javascripts/main.js
+++ javascripts/main.js
@@ -191,8 +191,10 @@ let buildNewObj = (element) => {
 		mdb: `${element.mdb}`,
 		uid: user.getUser()
 	};
-	if(element.poster_path){
+	if(element.poster_path != 'null'){
 		newObj.poster = `${movieDB.getMDBsettings().posterURL}${element.poster_path}`;
+	}else {
+		newObj.poster = "http://img02.deviantart.net/877e/i/2012/328/d/c/derpy_in_a_hole_by_uxyd-d5lz63l.png";
 	}
 	return newObj;
 };
